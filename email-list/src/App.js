8359824b456331360b81600f745598cf386dd57f
Byron <jamesblocker81@gmail.com>
2018-03-29 10:58:14 -0700

    add form

--- email-list/src/App.js
+++ email-list/src/App.js
@@ -1,18 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  
+  state = {
+    signups: [
+      {name: "Philippe", email: "phil@gmail.com"},
+      {name: "James", email: "james@gmail.com"},
+      {name: "Mahima", email: "mahima@gmail.com"}
+    ]
+  }
+
+  handleFormSubmit(evt) {
+    evt.preventDefault()
+    const {name, email} = this.refs
+    console.log(name.value)
+
+    const newSignup = {name: name.value, email: email.value}
+    console.log(newSignup)
+    this.setState({
+      signups: [newSignup, ...this.state.signups]
+    })
+
+    name.value=""
+    email.value=""
+    name.focus()
+  }
+  
+  
   render() {
+    const { signups } = this.state
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <form onSubmit={this.handleFormSubmit.bind(this)}>
+          <input ref="name" type="text" placeholder="Name"/>
+          <input ref="email" type="text" placeholder="Email"/>
+          <button>Sign Up</button>          
+        </form>
+
+        <h3>Number of Signups: {signups.length}</h3>
+        <ul>
+          {signups.map((person, index) => {
+            return (
+              <li key={index} onClick={() => { console.log(person) }}>
+                {person.name}
+              </li>
+            )
+          })}        
+        </ul>
+
       </div>
     );
   }
